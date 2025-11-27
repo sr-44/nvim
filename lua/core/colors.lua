309cbde3340b9efb8a128b2fda035ef989e59e59
sr-44 <mail.sr.44@gmail.com>
2025-11-27 19:29:36 +0500

    switched to catppuccin-mocha-blue color scheme

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -1,37 +1,38 @@
-
--- Add the selected color scheme in the `require` values below.
--- Current available color schemes: onedark, monokai, rose-pine.
-local status_ok, color_scheme = pcall(require, 'monokai')
-if not status_ok then
-  return
-end
-
--- Note: The instruction to load the color scheme may vary.
--- See the README of the selected color scheme for the instruction
--- to use.
--- e.g.: require('color_scheme').setup{}, vim.cmd('color_scheme') ...
-local monokai = require('monokai')
-local palette = monokai.pro
-monokai.setup {
-    custom_hlgroups = {
+-- Setup Catppuccin with Mocha flavor
+local status_ok, catppuccin = pcall(require, 'catppuccin')
+if status_ok then
+  catppuccin.setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    color_overrides = {
+      mocha = {
+        -- Можно переопределить цвета если нужно
+        -- blue = "#89b4fa", -- это стандартный синий в mocha
+      },
+    },
+    custom_highlights = function(colors)
+      return {
         TSInclude = {
-            fg = palette.aqua,
+          fg = colors.blue,
         },
         GitSignsAdd = {
-            fg = palette.green,
-            bg = palette.base2
+          fg = colors.green,
+          bg = colors.surface0
         },
         GitSignsDelete = {
-            fg = palette.pink,
-            bg = palette.base2
+          fg = colors.red,
+          bg = colors.surface0
         },
         GitSignsChange = {
-            fg = palette.orange,
-            bg = palette.base2
+          fg = colors.peach,
+          bg = colors.surface0
         },
-    }
-}
--- require('monokai').load()
+      }
+    end
+  })
+
+  -- Применяем цветовую схему
+  vim.cmd.colorscheme "catppuccin"
+end
 
 --[[
 Statusline color schemes.
@@ -39,7 +40,7 @@ Import the following color schemes in your statusline.lua file
 with: `require('core/colors').colorscheme_name` where the colors scheme name
 is the value of the variables below.
 
-e.g.: `local colors = require('core/colors').onedark_dark
+e.g.: `local colors = require('core/colors').catppuccin_mocha
 See: `core/statusline.lua`
 
 The color schemes below are created by following the "palette" file color
@@ -86,6 +87,21 @@ M.rose_pine = {
   yellow = '#f6c177',
   orange = '#2a2837',
   red = '#ebbcba',
+}
+
+-- Theme: Catppuccin (mocha)
+-- Colors: https://github.com/catppuccin/nvim
+-- Mocha palette with emphasis on blue
+M.catppuccin_mocha = {
+  bg = '#1e1e2e',
+  fg = '#cdd6f4',
+  pink = '#f5c2e7',
+  green = '#a6e3a1',
+  cyan = '#89dceb',
+  yellow = '#f9e2af',
+  orange = '#fab387',
+  red = '#f38ba8',
+  blue = '#89b4fa', -- основной синий цвет
 }
 
 return M
--- lua/core/statusline.lua
+++ lua/core/statusline.lua
@@ -11,7 +11,7 @@ if not status_ok then
 end
 
 -- Set colorscheme (from core/colors.lua/colorscheme_name)
-local colors = require('core/colors').monokai
+local colors = require('core/colors').catppuccin_mocha
 
 local vi_mode_colors = {
   NORMAL = colors.cyan,
